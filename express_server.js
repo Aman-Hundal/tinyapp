26c898494d8152005ecec5044504fc6d1c2cda5f
Aman Hundal <64342261+KewlKewlKewl@users.noreply.github.com>
2021-09-27 21:46:38 +0000

    added additional routes to example server

--- express_server.js
+++ express_server.js
@@ -11,6 +11,14 @@ app.get("/", (req, res) => { // resgisters a handler on the root path
   res.send("Hello!");
 });
 
+app.get("/urls.json", (req, res) => { //adds additional end points to our example_app. This one sends a json object
+  res.json(urlDatabase);
+});
+
+app.get('/hello', (req, res) => { //sends html code over through http. notice the <b> tag, the browser will pick up on this and bold the word world
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
