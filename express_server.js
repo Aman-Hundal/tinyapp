45748fdac75ebaee466218333fa34a71a973473f
Aman Hundal <64342261+KewlKewlKewl@users.noreply.github.com>
2021-10-01 06:27:03 +0000

    house keeping on code. Beta v1.0 ready

--- express_server.js
+++ express_server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-// const cookieParser = require('cookie-parser');
 const cookieSession = require('cookie-session');
 const app = express();
-const PORT = 8080; // default port 8080
 const bcrypt = require('bcryptjs');
 const bcryptjs = require("bcryptjs");
-const { generateRandomString, findUserByEmail, urlsForUser} = require('./helper');
-const urlDatabase = {//used to keep track of all the urls and their shortened forms. This is the data we ll want to show on the urls page.Keys look auto generated and the values are the long urls
+const { generateRandomString, findUserByEmail, urlsForUser} = require('./helper'); //helper functions used in app.
+
+const PORT = 8080;
+const urlDatabase = {
   b6UTxQ: {
     longURL: "https://www.tsn.ca",
     userID: "A128"
@@ -29,18 +29,18 @@ const userDatabase = {
     password: "123"
   }
 };
-app.set('view engine', 'ejs'); // to set EJS as the templating engine for the file.
 
+app.set('view engine', 'ejs');
 app.use(cookieSession({
   name: 'UserId',
   keys: ["great save luongo"]
 }));
-// app.use(cookieParser()); // allows you to Parse Cookie header and populate req.cookies with an object keyed by the cookie names.
-app.use(bodyParser.urlencoded({extended: true})); //middleware that will convert the request body from a buffer into a string/JS Object. it will then add the data to the req object under the key body. This will take the form data from the req.obj that i sent via post in the urls_new page and convert it into human language and not buffer language making it easy to find longURl to add to your DB.
+app.use(bodyParser.urlencoded({extended: true}));
 
+//HTTP ROUTES
 app.get('/', (req, res) => {
-  const cookieID = req.session.UserId;  
-  if(!cookieID) {
+  const cookieID = req.session.UserId;
+  if (!cookieID) {
     res.redirect('/login');
   }
   res.redirect('/urls');
@@ -48,55 +48,54 @@ app.get('/', (req, res) => {
 
 app.get('/urls', (req, res) => {
   const cookieID = req.session.UserId;
-  const userURLS = urlsForUser(cookieID, urlDatabase)
+  const userURLS = urlsForUser(cookieID, urlDatabase);
   
-  if(!cookieID) {
+  if (!cookieID) {
     res.status(401);
   }
 
-  const templateVars = {urls: userURLS, userObj: userDatabase[cookieID], cookieID }; // When sending variables to an EJS template, we need to send them inside an object, even if we are only sending one variable. This is so we can use the key of that variable (in the above case the key is urls) to access the data within our template.
-  res.render('pages/urls_index', templateVars); // this passes the urldatabase object data to our urls_index ESJ Temaplte. This will link the two data pieces
+  const templateVars = {urls: userURLS, userObj: userDatabase[cookieID], cookieID };
+  res.render('pages/urls_index', templateVars);
 });
 
-app.get('/urls/new', (req, res) => { //creates a GET route to return/render the form page to the client/browser/user. This has to be before the :id route because routes go linearlly up and down. If this was below the :id url, any calls to this urls/new page will be handle by :id because express will think that new is a route param.
+app.get('/urls/new', (req, res) => {
   const cookieID = req.session.UserId;
   const templateVars = { userObj: userDatabase[cookieID] };
 
-  if(!cookieID) {
+  if (!cookieID) {
     return res.status(401).redirect("/login");
-  };
+  }
 
   res.render('pages/urls_new', templateVars);
 });
 
-app.post('/urls', (req,res) => { //this will accept the post method/request from the url_new page and the form data it has to offer. This data (due the post method) will be sent in the body of the form/post request under the key longURL (name attribute in the input field). THe middleware makes the buffer lanugagre readable/into text and can take this data, acees the longUrl key and manipulate (add it to db, make a shortURL etc) to how we want using JS.
+app.post('/urls', (req,res) => {
   const cookieID = req.session.UserId;
   const longURL = req.body.longURL;
 
-  if(!cookieID) {
+  if (!cookieID) {
     return res.status(401).send("You are not a member. Please login or register to create a tiny URL.");
   }
 
-  if(!longURL) {
+  if (!longURL) {
     return res.status(403).send("Please do not leave URL fields blank.");
   }
 
   const shortURL = generateRandomString();
-  urlDatabase[shortURL]= {longURL: req.body.longURL, userID: cookieID};
-  console.log(urlDatabase)
-  res.redirect(`/urls/${shortURL}`); //add a redirect here -> string interpolate the shroturl to the end of our url so its /urls/shortUrl > then below will pick up on this.
-}); 
+  urlDatabase[shortURL] = {longURL: req.body.longURL, userID: cookieID};
+  res.redirect(`/urls/${shortURL}`);
+});
 
-app.get('/urls/:shortURL', (req, res) => { //the :shortURL makes the value after : a route parameter. This makes the value of this routeParam after : can be accessesd by the request (req) object using request.params.routeParam
+app.get('/urls/:shortURL', (req, res) => {
   const cookieID = req.session.UserId;
-  const shortURL = req.params.shortURL; //we can access the passed in url data after : as its stored in req.params.
+  const shortURL = req.params.shortURL;
   const shortURLObj = urlDatabase[shortURL];
   
-  if(urlDatabase[shortURL] === undefined) {
+  if (urlDatabase[shortURL] === undefined) {
     return res.status(404).send(`ERROR. ShortURL ID ${shortURL} does not exist.`);
   }
 
-  if(!cookieID) {
+  if (!cookieID) {
     res.status(401);
   }
 
@@ -104,17 +103,17 @@ app.get('/urls/:shortURL', (req, res) => { //the :shortURL makes the value after
     res.status(401);
   }
   
-  const templateVars = { shortURL: shortURL, longURL: urlDatabase[shortURL]['longURL'], userObj: userDatabase[cookieID], cookieID, shortURLObj }; // we are pssing over the shortURL info from the passed in route param (:urldata) and then the longUrl using that same shortUrl to the urls_show template
+  const templateVars = { shortURL: shortURL, longURL: urlDatabase[shortURL]['longURL'], userObj: userDatabase[cookieID], cookieID, shortURLObj };
   res.render('pages/urls_show', templateVars);
 });
 
-app.post('/urls/:shortURL/delete', (req, res) => { //route that listens and responds to request for the /delete page. This code picks up the request, deletes the passed throuhgh URL  and redirects the person to main page.
+app.post('/urls/:shortURL/delete', (req, res) => {
   const shortURL = req.params.shortURL;
   const cookieID = req.session.UserId;
   const shortURLObj = urlDatabase[shortURL];
 
-  if(!cookieID) {
-    return res.status(401).send("You are not a member. Please log in or register in order to delete tiny URL's.")
+  if (!cookieID) {
+    return res.status(401).send("You are not a member. Please log in or register in order to delete tiny URL's.");
   }
 
   if (cookieID !== shortURLObj["userID"]) {
@@ -122,21 +121,20 @@ app.post('/urls/:shortURL/delete', (req, res) => { //route that listens and resp
   }
 
   delete urlDatabase[shortURL];
-  console.log(urlDatabase)
   res.redirect('/urls');
 });
 
-app.post('/urls/:shortURL', (req, res) => { //route that waits for a request from the /edits page. This code picks up the request, takes in the shortURL realted to the request, picks up the req objects body info (which is created with the input elements name attrivute) whch is a new url and then changes the short url to the new longurl using object notation.
+app.post('/urls/:shortURL', (req, res) => {
   const shortURL = req.params.shortURL;
   const updatedURL = req.body.updatedURL;
   const shortURLObj = urlDatabase[shortURL];
   const cookieID = req.session.UserId;
 
-  if(!updatedURL) {
+  if (!updatedURL) {
     return res.status(403).send("Please do not leave URL fields blank.");
   }
 
-  if(!cookieID) {
+  if (!cookieID) {
     return res.status(401).send("You are not logged in. Please log in to make edits to tiny URLS.");
   }
 
@@ -145,7 +143,6 @@ app.post('/urls/:shortURL', (req, res) => { //route that waits for a request fro
   }
 
   urlDatabase[shortURL]['longURL'] = updatedURL;
-  console.log(urlDatabase)
   res.redirect('/urls');
 });
 
@@ -154,28 +151,21 @@ app.post('/login', (req,res) => {
   const password = req.body.password;
   const currentUser = findUserByEmail(email, userDatabase);
   
-  if(!currentUser) {
+  if (!currentUser) {
     return res.status(403).send("Email or password is incorrect. Please try again or register an account.");
   }
 
-  // console.log("current_user:", currentUser);
-
-  if(currentUser["email"] === email) {
-    bcrypt.compare(password, currentUser["password"])
-    .then((result) => {
-      if(result) {
-        req.session.UserId = currentUser["id"];
-        res.redirect('/urls');
-      } else {
-        return res.status(403).send("Email or password is incorrect. Please try again or register an account.");
-      }
-    })
-  };
-  // if(currentUser["email"] === email && currentUser["password"] === password) {
-  //   res.cookie("UserId", currentUser["id"]);
-  // } else {
-  //   return res.status(403).send("Password entered is incorrect. Please try again");
-  // }
+  if (currentUser["email"] === email) {
+    bcrypt.compare(password, currentUser["password"]) //ASYNC HASHING PASSWORD CHECK VIA PROMISES
+      .then((result) => {
+        if (result) {
+          req.session.UserId = currentUser["id"];
+          res.redirect('/urls');
+        } else {
+          return res.status(403).send("Email or password is incorrect. Please try again or register an account.");
+        }
+      });
+  }
 });
 
 app.get('/login', (req, res) => {
@@ -205,38 +195,35 @@ app.get('/register', (req,res) => {
   res.render('pages/urls_registration', templateVars);
 });
 
-app.post('/register', (req,res) => { //adding a POST route to access a newly registered user (see get route above) and their email and password. Then genrate an id and add the user to our userdatabase
+app.post('/register', (req,res) => {
   const id = generateRandomString();
   const email = req.body.email;
   const password = req.body.password;
 
-  if(!email || !password) {
+  if (!email || !password) {
     return res.status(400).send("Please do not leave email or password fields blank.");
   }
 
-  console.log(userDatabase)
-  console.log(email)
-  if(findUserByEmail(email, userDatabase)) { 
+  if (findUserByEmail(email, userDatabase)) {
     return res.status(400).send("This account already exists. Please try again.");
-  };
+  }
 
-  bcrypt.genSalt(10) //HASH ENCRYPTIN STEPP generate salt promise object
-  .then(function(salt) { //pass in salt promise object to callback function in .then
-    return bcrypt.hash(password, salt); // return hash promise object (by taking in salt promise obj result (10) and plaintext password above). and pass it to next then
-  })
-  .then(function(resultHash) { // take the rhash promise object from above, and unwrap its result (a hashed passowrd) into a new user object + pass in all the userdata generate above to the new user pbject
-    userDatabase[id] = {
-      id, //using ES6 logic to shorten property value notation
-      email, //same as id
-      password: resultHash //applying the result hash value returned from the .then and promises to the password proeprty for a user.
-    };
-    console.log("current DB:", userDatabase);
-    req.session.UserId = id;
-    res.redirect('/urls');
-  });
+  bcrypt.genSalt(10) //ASYNC PASSWORD HASHING VIA PROMISES
+    .then(function(salt) {
+      return bcrypt.hash(password, salt);
+    })
+    .then(function(resultHash) {
+      userDatabase[id] = {
+        id,
+        email,
+        password: resultHash
+      };
+      req.session.UserId = id;
+      res.redirect('/urls');
+    });
 });
 
-app.get('/u/:shortURL', (req, res) => { //new route to handle redirect links to longURL's
+app.get('/u/:shortURL', (req, res) => { //route to handle redirect links to longURL's
   const shortURL = req.params.shortURL;
 
   if (!urlDatabase[shortURL]) {
@@ -245,17 +232,8 @@ app.get('/u/:shortURL', (req, res) => { //new route to handle redirect links to 
   
   const longURL = urlDatabase[shortURL]["longURL"];
   res.redirect(longURL);
-  //when the browser receives a redirection repsonse it does another GET request to the url in the response (in this case the long URL and above to the other url pages we created)
 });
 
 app.listen(PORT, () => {
   console.log(`TinyApp listening on port ${PORT}!`);
 });
-
-// TO DO: 
-//code clean up code/order routes
-//ORDER ROUTES FROM MOST TO LEAST SPECIFIC
-//get routes in alphabetical then post in alpha
-
-
-// cookies issue for not logign out but shutting down server
--- test/helpersTest.js
+++ test/helpersTest.js
@@ -16,14 +16,13 @@ const testUsers = {
 
 describe('findUserByEmail', function() {
   it('should return a user with valid email', function() {
-    const user = findUserByEmail("user@example.com", testUsers)
+    const user = findUserByEmail("user@example.com", testUsers);
     const expectedOutput = testUsers["userRandomID"];
     assert.deepEqual(expectedOutput, user);
   });
   it('should return undefined value if no object found based on email ', function() {
-    const user = findUserByEmail("user123123@example.com", testUsers)
+    const user = findUserByEmail("user123123@example.com", testUsers);
     const expectedOutput = undefined;
     assert.equal(expectedOutput, user);
   });
 });
-
